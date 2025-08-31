d4819f7f686b5aab8c3b1a7a4c1da84e7070ff09
Abdslamekessou <151072580+Abdslamekessou@users.noreply.github.com>
2025-08-31 18:02:18 +0100

    Create Problem007.cpp

---
+++ Problem007.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+
+// 🚀 Problem: Write a program to reverse the digits of a given positive number.
+// ✅ Example: Input = 1234 → Output = 4321
+
+using namespace std;
+
+
+int ReadPositiveNumber(string Message) {
+
+    int Number;
+
+    do {
+        cout << Message <<endl;
+        cin >> Number;
+    } while (Number <= 0);
+
+
+    return Number;
+}
+
+
+
+int ReverseNumber(int Number){
+
+   int Remainder = 0 , reverseNum = 0;
+
+    while(Number > 0 ){
+
+       Remainder = Number % 10;
+       reverseNum = reverseNum * 10 + Remainder ;
+       Number = Number / 10;
+
+    }
+  
+    return reverseNum;
+}
+
+
+
+int main()
+{
+    int reverseNumber = ReverseNumber(ReadPositiveNumber("Please Enter A Positive Number?"));
+
+    cout <<"\nReverse is : " << reverseNumber <<endl ;
+
+    return 0;
+}
