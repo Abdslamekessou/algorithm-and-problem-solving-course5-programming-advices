890704bb027ba57cac6a0320fa30e80d9fdbc3cb
Abdslamekessou <151072580+Abdslamekessou@users.noreply.github.com>
2025-08-31 14:31:11 +0100

    Update Problem002.cpp

--- Problem002.cpp
+++ Problem002.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
-// 🔢 Program to print all prime numbers from 1 up to a user-specified number  
-// 🖨️ Checks each number and displays it if it is prime  
+// 🔢 Program to print all prime numbers from 1 up to a user-specified number
+// 🖨️ Checks each number and displays it if it is prime
 
 
 using namespace std;
 
-enum enPrimeOrNot  {Prime = 1 , NotPrime = 2 };
+enum enPrimeNotPrime {Prime = 1 , NotPrime = 2 };
 
 
 int ReadPositiveNumber(string Message) {
@@ -24,26 +24,30 @@ int ReadPositiveNumber(string Message) {
 
 
 
-enPrimeOrNot CheckPrime(int Number){
+enPrimeNotPrime CheckPrime(int Number){
 
-     for(int i = 2 ; i <= (Number/2) ; i++){
-         if(Number % i == 0 ){
-            return enPrimeOrNot::NotPrime;
+     for(int Counter = 2 ; Counter <= (Number/2) ; Counter++){
+         
+         if(Number % Counter == 0 ){
+            return enPrimeNotPrime::NotPrime;
          }
+         
      }
 
-   return enPrimeOrNot::Prime;
+   return enPrimeNotPrime::Prime;
 }
 
 
 
 void PrintPrimeNumbersFrom1ToN(int Number){
-    cout << "\n**Prime Numbers From 1 To 10**\n";
-    for(int i = 1 ; i <= Number ; i++){
+    cout << "\n** Prime Numbers From 1 To "<< Number << " **\n";
+    
+    for(int Counter = 1 ; Counter <= Number ; Counter++){
 
-        if(CheckPrime(i) == enPrimeOrNot::Prime){
-              cout << "\n" << i <<endl;
+        if(CheckPrime(Counter) == enPrimeNotPrime::Prime){
+              cout << "\n" << Counter <<endl;
         }
+        
      }
 
 }
@@ -52,7 +56,8 @@ void PrintPrimeNumbersFrom1ToN(int Number){
 
 int main()
 {
-    PrintPrimeNumbersFrom1ToN(ReadPositiveNumber("Please Enter A Number"));
+    PrintPrimeNumbersFrom1ToN(ReadPositiveNumber("Please Enter A Number?"));
 
     return 0;
 }
+
