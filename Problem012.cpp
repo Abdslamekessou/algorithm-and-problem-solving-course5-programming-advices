589b88357bb37fff8c4386f1b421e44512af13e1
Abdslamekessou <151072580+Abdslamekessou@users.noreply.github.com>
2025-09-01 17:48:57 +0100

    Create Problem012.cpp

---
+++ Problem012.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+// 🔢 Print an inverted number pattern from 1 up to i for each row  
+// 📝 Example: Input = 5  
+// Output:  
+// 1 2 3 4 5  
+// 1 2 3 4  
+// 1 2 3  
+// 1 2  
+// 1  
+
+
+using namespace std;
+
+
+int ReadPositiveNumber(string Message) {
+
+    int Number;
+
+    do {
+        cout << Message <<endl;
+        cin >> Number;
+    } while (Number <= 0);
+
+    return Number;
+}
+
+
+
+void printInvertedNumberPattern(int Number){
+    
+     cout << "\n";
+     
+     for(int i = Number ; i >= 1 ; i--){
+        
+        for(int j = 1 ; j <= i ; j++){
+            cout << j <<endl;
+        }
+        
+        cout << "\n";
+
+     }
+
+}
+
+
+
+int main()
+{
+    printInvertedNumberPattern(ReadPositiveNumber("Enter Number :"));
+    return 0;
+}
